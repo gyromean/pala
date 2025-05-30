11124d86887fd71a7375473bdcffee01530cb733
Pavel Holy <gyro1125@gmail.com>
2025-05-30 19:35:48 +0200

    make tab switch language direction

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -16,12 +16,52 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
   const prompt_text = Variable("")
   const mode_label = Variable("󰗊")
 
-  const results_content = Variable("Placeholder")
+  const langs = Variable(["en", "cs"])
+
+  // const results_content = Variable("Placeholder")
+  const results_content = Variable("Placeholder ".repeat(1000))
 
   let callback_handle = null
 
   let result_counter = 0
   let number_of_pending_asyncs = 0
+
+  const make_trans = () => {
+    if(callback_handle)
+      clearTimeout(callback_handle)
+    // prompt text is not empty
+    if(prompt_text.get()) {
+      callback_handle = setTimeout(() => {
+        let text_to_translate = prompt_text.get()
+        let result_id = ++result_counter;
+        number_of_pending_asyncs++
+        // execAsync(['trans', 'en:cs', text_to_translate])
+        const [lang_from, lang_to] = langs.get()
+        execAsync(['trans', `${lang_from}:${lang_to}`, text_to_translate])
+          .then((out) => {
+            number_of_pending_asyncs--
+            let should_quit = result_id < result_counter
+            if(number_of_pending_asyncs == 0) // reset result counter
+              result_counter = 0
+            if(should_quit)
+              return
+            let out_formatted = out.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '') // remove ansi escape characters
+            results_content.set(out_formatted)
+          })
+          .catch((err) => printerr(err))
+      }, 100)
+    }
+    // prompt text is empty
+    else {
+      result_counter++
+      results_content.set("")
+    }
+  }
+
+  const swap_langs = () => {
+    const [lang_from, lang_to] = langs.get()
+    langs.set([lang_to, lang_from])
+  }
 
   return <window
   className="Spotlight"
@@ -31,9 +71,17 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
   // css="background: green;"
   // keymode={Astal.Keymode.EXCLUSIVE}
   onKeyPressEvent={function (self, event: Gdk.Event) {
-    // ---------- exit ----------
-    if(event.get_keyval()[1] === Gdk.KEY_Escape)
-      App.quit()
+    print('keypress')
+    switch(event.get_keyval()[1]) {
+      case Gdk.KEY_Tab:
+        swap_langs()
+        make_trans()
+        return true
+        break
+      case Gdk.KEY_Escape:
+        App.quit()
+        break
+    }
   }}
   keymode={Astal.Keymode.ON_DEMAND}
   application={App}>
@@ -43,40 +91,19 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
           <label label={mode_label()}></label>
         </centerbox>
         <centerbox className="text">
-          <label label="en  cs"></label>
+          <label label={bind(langs).as(([lang_from, lang_to]) => {
+            print('zmena')
+            return `${lang_from}  ${lang_to}`
+          })}></label>
         </centerbox>
         <entry
         placeholderText="Search"
         enableEmojiCompletion={true}
         text={prompt_text()}
         onChanged={self => {
+          print('changed')
           prompt_text.set(self.text)
-          // ---------- trans ----------
-          if(callback_handle)
-            clearTimeout(callback_handle)
-          if(prompt_text.get()) {
-            callback_handle = setTimeout(() => {
-              let text_to_translate = prompt_text.get()
-              let result_id = ++result_counter;
-              number_of_pending_asyncs++
-              execAsync(['trans', 'en:cs', text_to_translate])
-                .then((out) => {
-                  number_of_pending_asyncs--
-                  let should_quit = result_id < result_counter
-                  if(number_of_pending_asyncs == 0) // reset result counter
-                    result_counter = 0
-                  if(should_quit)
-                    return
-                  let out_formatted = out.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '') // remove ansi escape characters
-                  results_content.set(out_formatted)
-                })
-                .catch((err) => printerr(err))
-            }, 100)
-          }
-          else {
-            result_counter++
-            results_content.set("")
-          }
+          make_trans()
         }}
         ></entry>
       </box>
